bfe7ee0c22d5bb38db686f6b2b6e54f51f10acba
singh-aryan-dev <aryandineshsingh1@gmail.com>
2025-10-23 23:03:09 +0530

    fix Ui

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -64,7 +64,7 @@ export const Navbar = () => {
 
         <div
           className={cn(
-            "fixed inset-0 bg-background/95 backdrop-blur-md z-40 flex flex-col items-center justify-center",
+            "fixed inset-0 bg-background/95 backdrop-blur-md z-40 flex flex-col items-center justify-center h-[100vh]",
             "transition-all duration-300 md:hidden",
             isMenuOpen
               ? "opacity-100 pointer-events-auto"
